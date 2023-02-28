a44f23b9e257b32e18704b78087f97262bef3bf9
Louis Poupat <louispoupat@sfr.fr>
2023-02-28 20:08:19 +0100

    fix CORS

--- go_app/web/handler.go
+++ go_app/web/handler.go
@@ -19,14 +19,10 @@ func NewHandler(store *database.Store) *Handler {
 	handler.Use(middleware.Logger)
 
 	handler.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		AllowCredentials: true,
+		AllowedOrigins:   []string{"*"},
+		AllowedHeaders:   []string{"X-PINGOTHER", "Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Token", "Access-Control-Allow-Origin"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 	}))
 
 	/* Authentification route */
