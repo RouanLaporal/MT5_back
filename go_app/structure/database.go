ff7619d2c485da9e8463b8ad6c46b12cdba87624
Feret-Jocelin <feret.jocelin@outlook.fr>
2023-03-01 13:17:06 +0100

    organize struct and interface

--- go_app/structure/database.go
+++ go_app/structure/database.go
@@ -22,32 +22,6 @@ type Shop struct {
 	UserID      int    `json:"id_user"`
 }
 
-type Kind struct {
-	ID   int    `json:"id_kind"`
-	Name string `json:"name"`
-}
-
-type Benefit struct {
-	IDBenefit   int    `json:"id_benefit"`
-	IDShop      int    `json:"id_shop"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Duration    string `json:"duration"`
-	Price       string `json:"price"`
-}
-
-type Review struct {
-	IDReview int    `json:"id_review"`
-	IDShop   int    `json:"id_shop"`
-	IDUser   int    `json:"id_user"`
-	Rating   int    `json:"rating"`
-	Comment  string `json:"comment"`
-}
-
-type KindStoreInterface interface {
-	GetAllKind() (Kind, error)
-}
-
 type ShopStoreInterface interface {
 	// GetAllShopByKindAndPosition(id_type int) ([]Shop, error)
 	GetAllShopByKindAndCity(id_kind int, city string) ([]Shop, error)
@@ -55,18 +29,4 @@ type ShopStoreInterface interface {
 	AddShop(shop Shop) (int, error)
 	DeleteShop(id int) error
 	UpdateShop(id int, item Shop) error
-}
-
-type BenefitStoreInterface interface {
-	GetBenefitByShop(id_shop int) ([]Benefit, error)
-	AddBenefit(benefit Benefit) (int, error)
-	UpdateBenefit(id_benefit int, item Benefit) error
-	DeleteBenefit(id_benefit int) error
-}
-
-type ReviewStoreInterface interface {
-	GetReviewByShop(id_shop int) ([]Review, error)
-	AddReview(review Review) (int, error)
-	UpdateReview(id_review int, item Review) error
-	DeleteReview(id_review int) error
 }
--- go_app/structure/shop_structure.go
+++ go_app/structure/shop_structure.go
@@ -8,3 +8,34 @@ type Kind struct {
 type KindStoreInterface interface {
 	GetAllKind() ([]Kind, error)
 }
+
+type Benefit struct {
+	IDBenefit   int    `json:"id_benefit"`
+	IDShop      int    `json:"id_shop"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Duration    string `json:"duration"`
+	Price       string `json:"price"`
+}
+
+type BenefitStoreInterface interface {
+	GetBenefitByShop(id_shop int) ([]Benefit, error)
+	AddBenefit(benefit Benefit) (int, error)
+	UpdateBenefit(id_benefit int, item Benefit) error
+	DeleteBenefit(id_benefit int) error
+}
+
+type Review struct {
+	IDReview int    `json:"id_review"`
+	IDShop   int    `json:"id_shop"`
+	IDUser   int    `json:"id_user"`
+	Rating   int    `json:"rating"`
+	Comment  string `json:"comment"`
+}
+
+type ReviewStoreInterface interface {
+	GetReviewByShop(id_shop int) ([]Review, error)
+	AddReview(review Review) (int, error)
+	UpdateReview(id_review int, item Review) error
+	DeleteReview(id_review int) error
+}
